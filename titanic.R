8d5ec0cbf98ef390f3fd035486771abc04f8d54b
Matthias Kutz <kutz.matthias@gmail.com>
2018-07-09 00:30:49 +0200

    Add creation of prediction for submission

--- titanic.R
+++ titanic.R
@@ -12,8 +12,11 @@ data <- read.csv("data/train.csv")
 # make "Survived" a categorial variable
 data$Survived <- as.factor(data$Survived)
 
+# seed for reproducibility
+seed <- 42
+
 # split and import datasets to h2o
-split <- h2o.splitFrame(as.h2o(data), ratios = 0.75)
+split <- h2o.splitFrame(as.h2o(data), ratios = 0.75, seed = seed)
 
 train <- split[[1]]
 valid <- split[[2]]
@@ -28,6 +31,7 @@ m <- h2o.randomForest(x = x,
                      y = y,
                      training_frame = train,
                      validation_frame = valid,
+                     seed = seed,
                      model_id = "titanic_rf_defaults")
 
 # create model plots
@@ -36,3 +40,12 @@ ggsave("rf_defaults_model_scoring_history.png", plot_scoring_history(m), "png", 
 
 # save model
 h2o.saveModel(m, "output/models", force = TRUE)
+
+# predict on test data
+prediction <- h2o.predict(m, test)
+
+# create output csv 
+output <- cbind(as.vector(test$PassengerId), as.vector(prediction$predict))
+colnames(output) <- c("PassengerId", "Survived")
+write.csv(output, "output/predictions/rf_defaults.csv", row.names = FALSE)
+
